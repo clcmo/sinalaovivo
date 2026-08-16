870b7a17565e3b866604c7b6fac98bf8b3697cb4
Camila Leite (Milla) <ola@camilaloliveira.com>
2026-08-16 18:08:59 -0300

    feat: Update config.js

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@ export const CONFIG = {
   // Crie em https://console.cloud.google.com/apis/credentials
   // Tipo: "ID do cliente OAuth" > Aplicativo da Web
   // Origens JavaScript autorizadas: a URL onde o GitHub Pages vai publicar o site
-  GOOGLE_CLIENT_ID: 'SEU_CLIENT_ID_AQUI.apps.googleusercontent.com',
+  GOOGLE_CLIENT_ID: '548410040077-6jriphfld2thua9426r68nmbfqj0e2ce.apps.googleusercontent.com',
 
   // Escopo somente-leitura: dá acesso de busca/consulta na API do YouTube
   // em nome de quem fizer login, sem poder alterar nada na conta da pessoa.
